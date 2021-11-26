e503e6d5ed508ed706f77eaf1fdb1ea62275c276
Jesmin Sultana <hiraa.iubat@gmail.com>
2021-11-27 00:32:44 +0600

    user part updated

--- resources/views/admin/layouts/user_form.blade.php
+++ resources/views/admin/layouts/user_form.blade.php
@@ -26,7 +26,21 @@
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter Your Problem Type</label>
-    <input name="problem_type" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <select name="problem_type" id="problem_type">
+    <option value="electricity">Electricity</option>
+    <option value="water">Water</option>
+    <option value="gas">Gas</option>
+    <option value="street">Street</option>
+    <option value="street">Mosquito</option>
+    <option value="street">Waterlogging</option>
+    <option value="street">Garbage</option>
+    <option value="street">Sewage</option>
+
+
+
+
+  </select>
+    <!-- <input name="problem_type" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> -->
   </div>
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Enter Your Application Type</label>
